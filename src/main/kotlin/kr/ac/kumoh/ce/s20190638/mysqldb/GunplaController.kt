2474823893fb97a21091aa46bf7fb8484a0bf3ad
GIDONG <sgd96012@naver.com>
2024-06-17 19:09:29 +0900

    todo complete

--- src/main/kotlin/kr/ac/kumoh/ce/s20190638/mysqldb/GunplaController.kt
+++ src/main/kotlin/kr/ac/kumoh/ce/s20190638/mysqldb/GunplaController.kt
@@ -1,14 +1,16 @@
 package kr.ac.kumoh.ce.s20190638.mysqldb
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 class GunplaController(val service: MechanicService) {
     @GetMapping("/")
-    fun welcom() : String {
-        return "sex"
+    fun welcome() : String {
+        return "hello"
     }
 
     @GetMapping("/mechanics")
@@ -16,21 +18,17 @@ class GunplaController(val service: MechanicService) {
         return service.getAllMechanics()
     }
 
-    @GetMapping("/mechanics/add")
-    fun addMechanic(): Map<String, Any> {
+    @PostMapping("/mechanics/add")
+    fun addMechanic(@RequestBody newMechanic: Mechanic): Map<String, Any> {
         val result = HashMap<String, Any>()
-        try {
-            //TODO : 하드코딩 대신 데이터 받아서 추가하도록 변경할것
-            // @PostMapping 사용
-            service.add(
-                Mechanic(0, "네러티브 건담", "RX-9", "Anaheim Electronics", "Gundarium Alloy",21,40)
-            )
+        return try {
+            service.add(newMechanic)
             result["result"] = "success"
-            return result
+            result
         } catch (e: Exception) {
             result["result"] = "failed"
-            result["message"] = e.toString()
-            return result
+            result["message"] = e.message.toString()
+            result
         }
     }
 
